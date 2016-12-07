ce9be457dc330f25e2d700149bf12837f557746a
AaronFogarty <aaron.fogarty@gmail.com>
2016-12-07 18:31:16 +0000

    accordion added angular not working

---
+++ js/resultAccordion.js
@@ -0,0 +1,7 @@
+    $(function() {
+$("#result_accordion").accordion({ collapsible: true, 
+			                active: false, 
+							heightStyle: "content",
+							autoHeight: false,
+							clearStyle: true });
+    });
