33391889902d85c1d23448450b1f5cac06df3fde
AaronFogarty <aaron.fogarty@gmail.com>
2016-12-07 14:30:03 +0000

    modal contact form and links added

---
+++ js/results.js
@@ -0,0 +1,134 @@
+var education = angular.module("education", []);
+
+    education.controller("educationController", 
+		function educationController($scope) {
+			// Array of my subjects and results added
+			$scope.education = {
+firstYear: [
+{
+id: 1,
+subject: "First Year",
+grade: "Pass"
+},
+{
+id: 2,
+subject: "Project",
+grade: "A"
+},
+{
+id: 3,
+subject: "Advanced Databases",
+grade: "A"
+},
+{
+id: 4,
+subject: "Cloud Services",
+grade: "B+"
+},
+{
+id: 5,
+subject: "Innovation",
+grade: "B+"
+},
+{
+id: 6,
+subject: "Operation Systems",
+grade: "A"
+},
+{
+id: 7,
+subject: "Web Design 2",
+grade: "A"
+},
+{
+id: 8,
+subject: "Algorithms and Data Structures",
+grade: "A"
+}
+				],
+secondYear: [
+{
+id: 1,
+subject: "2nd Year",
+grade: "Pass"
+},
+{
+id: 2,
+subject: "Project",
+grade: "A"
+},
+{
+id: 3,
+subject: "Advanced Databases",
+grade: "A"
+},
+{
+id: 4,
+subject: "Cloud Services",
+grade: "B+"
+},
+{
+id: 5,
+subject: "Innovation",
+grade: "B+"
+},
+{
+id: 6,
+subject: "Operation Systems",
+grade: "A"
+},
+{
+id: 7,
+subject: "Web Design 2",
+grade: "A"
+},
+{
+id: 8,
+subject: "Algorithms and Data Structures",
+grade: "A"
+}
+				],
+thirdYear: [
+{
+id: 1,
+subject: "3rd year",
+grade: "Pass"
+},
+{
+id: 2,
+subject: "Project",
+grade: "A"
+},
+{
+id: 3,
+subject: "Advanced Databases",
+grade: "A"
+},
+{
+id: 4,
+subject: "Cloud Services",
+grade: "B+"
+},
+{
+id: 5,
+subject: "Innovation",
+grade: "B+"
+},
+{
+id: 6,
+subject: "Operation Systems",
+grade: "A"
+},
+{
+id: 7,
+subject: "Web Design 2",
+grade: "A"
+},
+{
+id: 8,
+subject: "Algorithms and Data Structures",
+grade: "A"
+}
+				]				
+			};
+    });
